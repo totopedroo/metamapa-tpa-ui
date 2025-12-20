9576a465c9ea61c8cf09b3795045d7e62269615e
LucasVerdaguer <lverdaguer@frba.utn.edu.ar>
2025-12-20 18:36:38 -0300

    Visualizacion en colecciones, revisar el css

--- src/main/java/ar/utn/ba/ddsi/metamapa/controllers/HechoUiApiController.java
+++ src/main/java/ar/utn/ba/ddsi/metamapa/controllers/HechoUiApiController.java
@@ -4,10 +4,7 @@ import ar.utn.ba.ddsi.metamapa.dto.HechoDTO;
 import ar.utn.ba.ddsi.metamapa.services.HechosUiService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Map;
@@ -32,4 +29,6 @@ public class HechoUiApiController {
       return ResponseEntity.internalServerError().body(Map.of("error", e.getMessage()));
     }
   }
+
+
 }
